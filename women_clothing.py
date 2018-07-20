9dad590c74c00fab81a0d91063aa3d8272f79f7c
ryan-kttam <ryantam0626@gmail.com>
2018-07-19 22:14:17 -0700

    added exploratory analysis - Graph

--- women_clothing.py
+++ women_clothing.py
@@ -6,3 +6,18 @@ data = data.rename(columns={list(data)[0] : 'count'})
 data.head()
 
 data.describe()
+
+dept_vs_rating = data.groupby(['Department Name', 'Rating']).count()
+
+
+import seaborn as sns
+
+sns.countplot(x='Department Name', hue='Rating',data=data)
+
+sns.countplot(x='Rating', hue='Recommended IND',data=data)
+
+sns.countplot(x='Department Name', hue='Recommended IND',data=data)
+
+# working
+sns.barplot(x='Department Name',y='prop',hue='Recommended IND',data=data['Department Name'].groupby(data['Recommended IND']).value_counts(normalize=True).rename('prop').reset_index())
+
